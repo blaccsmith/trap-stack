39c7028d1353f62ffaa6503417cfcc93718c816c
Darryl Brooks <darrylbrooks13@gmail.com>
2022-03-21 18:12:16 -0500

    fix: fixing typo

--- remix.config.js
+++ remix.config.js
@@ -3,7 +3,7 @@
  */
 module.exports = {
   serverBuildTarget: "vercel",
-  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.ts",
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: [".*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
 };
