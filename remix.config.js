157087877ad4ddd75312fc58165735637b63c36a
Darryl Brooks <darrylbrooks13@gmail.com>
2022-03-22 20:59:25 -0500

    chore: removing server config

--- remix.config.js
+++ remix.config.js
@@ -3,7 +3,6 @@
  */
 module.exports = {
   serverBuildTarget: "vercel",
-  server: process.env.NODE_ENV === "development" ? undefined : "./server.ts",
   serverBuildPath: "public/build/index.js",
   ignoredRouteFiles: [".*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
 };
