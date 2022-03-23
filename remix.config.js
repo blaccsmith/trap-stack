f9b219eef0563530a4c3208c2986e3521de880f9
Darryl Brooks <darrylbrooks13@gmail.com>
2022-03-22 22:39:27 -0500

    chore: minor updates

--- remix.config.js
+++ remix.config.js
@@ -3,6 +3,7 @@
  */
 module.exports = {
   serverBuildTarget: "vercel",
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.ts",
   serverBuildPath: "public/build/index.js",
   ignoredRouteFiles: [".*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
 };
